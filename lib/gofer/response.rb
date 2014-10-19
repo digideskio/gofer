f3a07c06d4280fc86ad984c824df00c00405b62b
Jordon Bedwell <jordon@envygeeks.io>
2014-10-19 10:12:11 -0500

    Weird spacing.

--- lib/gofer/response.rb
+++ lib/gofer/response.rb
@@ -1,7 +1,7 @@
 module Gofer
   class Response < String
     attr_reader :stdout, :stderr, :output, :exit_status
-    def initialize(stdout,  stderr,  output,  exit_status)
+    def initialize(stdout, stderr, output, exit_status)
       super @stdout = stdout and (
         @stderr, @output, @exit_status = stderr, output, exit_status
       )
