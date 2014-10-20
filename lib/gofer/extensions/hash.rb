8f2c4b7d805beeade3d1d2eef3becededecc4758
Jordon Bedwell <jordon@envygeeks.io>
2014-10-20 10:49:10 -0500

    Add #symbolize_keys and #elegant_merge!

--- lib/gofer/extensions/hash.rb
+++ lib/gofer/extensions/hash.rb
@@ -1,4 +1,17 @@
 class Hash
+  def elegant_merge(hash)
+    merge(hash) do |k, ov, nv|
+      ov.is_a?(Hash) && nv.is_a?(Hash) ? ov.elegant_merge(nv) : nv
+    end
+  end
+
+  def symbolize_keys
+    inject({}) do |h, (k, v)|
+      # I am not in the business of messing with non-String object keys.
+      h.update(k.is_a?(String) ? k.to_sym : k => v.is_a?(Hash) ? v.symbolize_keys : v)
+    end
+  end
+
   def merge_if(hash)
     self.merge(hash) do |k, ov, nv|
       if nv.is_a?(Hash) && ov.is_a?(Hash)
