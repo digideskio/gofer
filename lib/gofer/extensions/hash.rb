b7ec15c936c634a7a5d67ce9d27b04110b01ccb7
Jordon Bedwell <jordon@envygeeks.io>
2014-10-31 11:57:55 -0500

    Update hash further.

--- lib/gofer/extensions/hash.rb
+++ lib/gofer/extensions/hash.rb
@@ -1,8 +1,16 @@
 class Hash
-  def deep_merge!(hash)
-    merge!(hash) do |key, ovalue, nvalue|
-      ovalue.is_a?(Hash) && nvalue.is_a?(Hash) ? ovalue.deep_merge!(nvalue) : nvalue
+  def deep_merge(hash)
+    merge(hash) do |key, ovalue, nvalue|
+      if ovalue.respond_to?(:deep_merge) && nvalue.respond_to?(:deep_merge)
+        ovalue.deep_merge(nvalue)
+      else
+        nvalue
+      end
     end
+  end
+
+  def deep_merge!(hash)
+    replace(deep_merge(hash))
   end
 
   def stringize!
@@ -17,13 +25,17 @@ class Hash
     end
   end
 
-  def merge_if!(hash)
-    merge!(hash) do |key, ovalue, nvalue|
-      if nvalue.is_a?(Hash) && ovalue.is_a?(Hash)
-        ovalue.merge_if!(nvalue)
+  def merge_if(hash)
+    merge(hash) do |key, ovalue, nvalue|
+      if ovalue.respond_to?(:merge_if) && nvalue.respond_to?(:merge_if)
+        ovalue.merge_if(nvalue)
       else
-        ! has_key?(key) || ovalue.nil? ? nvalue : ovalue
+        ! has_key?(key) ? nvalue : ovalue
       end
     end
+  end
+
+  def merge_if!(hash)
+    replace(merge_if(hash))
   end
 end
